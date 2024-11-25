dcd490d62fa192d8f3d976909ca9be4a7d48fc1c
faigygit <faigy2549@gmail.com>
2024-11-25 18:38:57 +0200

    initial commit'

---
+++ codeBundler/Commands/BundleCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine.Invocation;
+using System.CommandLine;
+using codeBundler.Services;
+
+
+namespace codeBundler.Commands
+{
+    public static class BundleCommand
+    {
+        public static Command CreateCommand()
+        {
+            var command = new Command("bundle", "Bundle code files into a single file.");
+
+            var languageOption = new Option<string>(
+                new[] { "--language", "-l" },
+                "Programming language to include (e.g., cs, js, all)");
+            languageOption.IsRequired = true;
+            command.AddOption(languageOption);
+
+            var outputOption = new Option<string?>(
+                new[] { "--output", "-o" },
+                "Output path for the bundled file (default: 'bundle.txt').");
+            command.AddOption(outputOption);
+
+            var noteOption = new Option<bool>(
+                new[] { "--note", "-n" },
+                "Include file names and paths as comments.");
+            command.AddOption(noteOption);
+
+            var sortOption = new Option<string>(
+                new[] { "--sort", "-s" },
+                () => "filename",
+                "Sorting order (filename or type).");
+            command.AddOption(sortOption);
+
+            var removeEmptyLinesOption = new Option<bool>(
+                new[] { "--remove-empty-lines", "-r" },
+                "Remove empty lines from code files.");
+            command.AddOption(removeEmptyLinesOption);
+
+            var authorOption = new Option<string?>(
+                new[] { "--author", "-a" },
+                "Include the author's name as a comment at the top.");
+            command.AddOption(authorOption);
+
+            // Bind the handler
+            command.SetHandler(
+             (string language, string? output, bool note, string sort, bool removeEmptyLines, string? author) =>
+             {
+                 BundlerService.BundleFiles(language, output, note, sort, removeEmptyLines, author);
+             },
+             languageOption, outputOption, noteOption, sortOption, removeEmptyLinesOption, authorOption);
+
+            return command;
+        }
+    }
+}
+
---
+++ codeBundler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using codeBundler.Commands;
+using System;
+
+var rootCommand = new RootCommand("CodeBundler CLI")
+{
+    BundleCommand.CreateCommand(),
+    CreateRspCommand.CreateCommand()
+};
+
+return await rootCommand.InvokeAsync(args);
